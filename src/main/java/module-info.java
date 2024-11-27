4770d92674df5c1b9e9e834cabdfc7649ebb943f
thorjessing <thorwinddk@gmail.com>
2024-11-27 10:39:31 +0100

    server info added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,6 @@ module com.example.spotify_clone {
 
     opens dk.easv.spotify_clone to javafx.fxml;
     exports dk.easv.spotify_clone;
-    exports dk.easv.spotify_clone.GUI;
-    opens dk.easv.spotify_clone.GUI to javafx.fxml;
     exports dk.easv.spotify_clone.GUI.Controller;
     opens dk.easv.spotify_clone.GUI.Controller to javafx.fxml;
 }
