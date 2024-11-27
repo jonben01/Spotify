731272fa8fe70fcb2d11215be83db20b156b7e96
Jonas Bendorff <jonben01@easv365.dk>
2024-11-27 13:09:06 +0100

    Defined song class, and made new class files

--- src/main/java/dk/easv/spotify_clone/BE/Song.java
+++ src/main/java/dk/easv/spotify_clone/BE/Song.java
@@ -1,4 +1,42 @@
 package dk.easv.spotify_clone.BE;
 
+
+
 public class Song {
+
+    private int songId;
+    private String songName;
+    private String songArtist;
+    private String songGenre;
+    private String songFilePath;
+
+    //Getters and setters for variables
+
+    public int getSongID() {return songId;}
+
+    public void setSongID(int songID) {this.songId = songID;}
+
+    public String getSongName() {return songName;}
+
+    public void setSongName(String songName) {this.songName = songName;}
+
+    public String getSongArtist() {return songArtist;}
+
+    public void setSongArtist(String songArtist) {this.songArtist = songArtist;}
+
+    public String getSongGenre() {return songGenre;}
+
+    public void setSongGenre(String songGenre) {this.songGenre = songGenre;}
+
+    public String getSongFilePath() {return songFilePath;}
+
+    public void setSongFilePath(String songFilePath) {this.songFilePath = songFilePath;}
+
+    public Song(int songID, String songName, String songArtist, String songGenre, String songFilePath) {
+        this.songId = songID;
+        this.songName = songName;
+        this.songArtist = songArtist;
+        this.songGenre = songGenre;
+        this.songFilePath = songFilePath;
+    }
 }
---
+++ src/main/java/dk/easv/spotify_clone/DAL/PlaylistDAO_db.java
@@ -0,0 +1,4 @@
+package dk.easv.spotify_clone.DAL;
+
+public class PlaylistDAO_db {
+}
